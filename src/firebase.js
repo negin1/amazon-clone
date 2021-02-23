bd144c29553d808f129928313c48d54ad68d1da8
negin1 <negin.jalalian@gmail.com>
2021-02-23 15:05:47 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBlnP1P5P_OFNOWb2cH_0uiuaEkjaX1bMI",
+  authDomain: "clone-a7b42.firebaseapp.com",
+  projectId: "clone-a7b42",
+  storageBucket: "clone-a7b42.appspot.com",
+  messagingSenderId: "228058572732",
+  appId: "1:228058572732:web:0f5d976c776da1f913adb9"
+};
