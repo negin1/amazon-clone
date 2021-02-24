d85ceb1206274a2d0dd2cd650dcef511566b5cb3
negin1 <negin.jalalian@gmail.com>
2021-02-24 09:56:08 +0100

    product page has been added

--- src/Home.js
+++ src/Home.js
@@ -13,18 +13,55 @@ function Home() {
               />
 
           <div className="home__row">
-                <Product />
-                <Product />
+                <Product 
+                  id="1"
+                  title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"
+                  price={29.99}
+                  image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+                  rating={5}
+                />
+                <Product 
+                 id="2"
+                  title="Kask Mojito X Unisex Road Helmet, unisex"
+                  price={122.94}
+                  image="https://m.media-amazon.com/images/I/91tl1wMvoTL._AC_UL320_.jpg"
+                  rating={8}
+                />
             </div>
 
           <div className="home__row">
-                <Product />
-                <Product />
-                <Product />
+                <Product 
+                 id="3"
+                  title="Fitbit Inspire 2 Health & Fitness Tracker with a Free 1-Year Fitbit Premium Trial, 24/7 Heart Rate & up to 10 Days Battery "
+                  price={83.49}
+                  image="https://m.media-amazon.com/images/I/61GjVzv-reL._AC_UY218_.jpg"
+                  rating={4}
+                />
+                <Product 
+                 id="4"
+                  title="Shark Corded Stick Vacuum Cleaner [HV390UK]"
+                  price={149.99}
+                  image="https://images-eu.ssl-images-amazon.com/images/I/618XS0kVyeL.__AC_SX300_SY300_QL70_ML2_.jpg"
+                  rating={3}
+                />
+                <Product 
+                 id="5"
+                  title="VonShef Personal Blender Multifunctional Powerful Smoothie Maker and Mixer for Fruit, Vegetables Shakes and Ice Includes 800ml and 500ml Portable Cups – 1000W"
+                  price={54.99}
+                  image="https://m.media-amazon.com/images/I/510OOQMrHPL._AC_SR320,320_.jpg"
+                  rating={5}
+                />
           </div>
 
            <div className="home__row">
-               <Product />
+              <Product 
+                 id="5"
+                  title="Samsung GQ32Q50RGUXZG TV 81.3 cm 4K Ultra HD Smart TV Wi-Fi Silver GQ32Q50RGUXZG, 81.3 cm , 3840 x 2160 pixels, QLED, Smart TV, Wi-Fi, Silver
+                  "
+                  price={616.64}
+                  image="https://m.media-amazon.com/images/I/61ifRN1QZDL._AC_UY218_.jpg"
+                  rating={5}
+                />
           </div>
         </div>
     </div>
